a137e12b3a57b9c7057756eb72683029fa40e766
Smokey <ghostking2033@gmail.com>
2022-05-07 13:28:31 +0300

    fix

--- WpfTilt/Mouse.cs
+++ WpfTilt/Mouse.cs
@@ -2,54 +2,54 @@
 
 namespace WpfTilt
 {
-	public sealed class Mouse : Devices, IDrawable
-	{
-		public int NumOfButtons { get; set; }
-		public string Backlight { get; set; }
-		public string Colour { get; set; }
-		public string PrintContent { get => Brand; }
-		public string this[int index] => "Mouse" + index;
-		public Mouse() : this("A4Tech", "COC100", 100, 3, "None", "Black")
-		{
+    public sealed class Mouse : Devices, IDrawable
+    {
+        public int NumOfButtons { get; set; }
+        public string Backlight { get; set; }
+        public string Colour { get; set; }
+        public string PrintContent { get => Brand; }
+        public string this[int index] => "Mouse" + index;
+        public Mouse() : this("A4Tech", "COC100", 100, 3, "None", "Black")
+        {
 
-		}
-		public Mouse(string _brand, string _model, int _price) : this(_brand, _model, _price, 3, "white", "black")
-		{
+        }
+        public Mouse(string _brand, string _model, int _price) : this(_brand, _model, _price, 3, "white", "black")
+        {
 
-		}
-		public Mouse(string _brand, string _model, int _price, int _numOfButtons, string _backlight, string _colour) : base(_brand, _model, _price)
-		{
-			NumOfButtons = _numOfButtons;
-			Backlight = _backlight;
-			Colour = _colour;
-		}
-		public void readFromFile(StreamReader sr)
-		{
-			this.Brand = sr.ReadLine();
-			this.Model = sr.ReadLine();
-			this.Price = int.Parse(sr.ReadLine());
-			this.Colour = sr.ReadLine();
-		}
-		public void writeToFile(StreamWriter sw)
-		{
-			sw.WriteLine(Brand);
-			sw.WriteLine(Model);
-			sw.WriteLine(Price);
-			sw.WriteLine(Colour);
-		}
-		public override string selfDescribe()
-		{
-			return "I am " + FullName + " mouse";
-		}
-		public void Print(MessageSender sender)
-		{
-			sender(PrintContent);
-		}
+        }
+        public Mouse(string _brand, string _model, int _price, int _numOfButtons, string _backlight, string _colour) : base(_brand, _model, _price)
+        {
+            NumOfButtons = _numOfButtons;
+            Backlight = _backlight;
+            Colour = _colour;
+        }
+        public void readFromFile(StreamReader sr)
+        {
+            this.Brand = sr.ReadLine();
+            this.Model = sr.ReadLine();
+            this.Price = int.Parse(sr.ReadLine());
+            this.Colour = sr.ReadLine();
+        }
+        public void writeToFile(StreamWriter sw)
+        {
+            sw.WriteLine(Brand);
+            sw.WriteLine(Model);
+            sw.WriteLine(Price);
+            sw.WriteLine(Colour);
+        }
+        public override string selfDescribe()
+        {
+            return "I am " + FullName + " mouse";
+        }
+        public void Print(MessageSender sender)
+        {
+            sender(PrintContent);
+        }
 
-		public void Draw(string something, MessageSender sender)
-		{
-			sender("Showed Device " + something);
-		}
+        public void Draw(string something, MessageSender sender)
+        {
+            sender("Showed Device " + something);
+        }
 
-	}
+    }
 }
--- WpfTilt/SystemUnit.cs
+++ WpfTilt/SystemUnit.cs
@@ -2,68 +2,69 @@
 
 namespace WpfTilt
 {
-	public class SystemUnit : IDrawable
-	{
-		public string Brand { get; set; }
-		public string Model { get; set; }
-		public int Price { get; set; }
-		public string GetUnitName {get{ return this.Brand + " " + this.Model; } }
-		public string this[int index] => "System Unit" + index;
-		public string Colour { get; set; }
-	public string CPU { get; set; }
-	public string Size { get; set; }
-	public string Motherboard { get; set; }
-	public int Ram { get; set; }
-	public string PrintContent { get => Brand; }
-		public SystemUnit() :this("Papuas","Venecia",300)
-		{
-			
-		}
-	public SystemUnit(string _brand,string _model, int _price):this(_brand,_model,_price,"black","intelcorei14.1","mid-tower","asusgromyako",32)
-		{
-
-		}
-	public SystemUnit(string _brand, string _model, int _price,string _colour,string _cpu,string _size,string _motherboard,int _ram)
+    public class SystemUnit : IDrawable
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public string GetUnitName { get { return this.Brand + " " + this.Model; } }
+        public string this[int index] => "System Unit" + index;
+        public string Colour { get; set; }
+        public string CPU { get; set; }
+        public string Size { get; set; }
+        public string Motherboard { get; set; }
+        public int Ram { get; set; }
+        public string PrintContent { get => Brand; }
+        public SystemUnit() : this("Papuas", "Venecia", 300)
         {
-			Brand = _brand;
-			Model = _model;
-			Price = _price;
-			Colour = _colour;
-			Size = _size;
-			Motherboard = _motherboard;
-			CPU = _cpu;
-			Ram = _ram;
-        }
-	public void readFromFile(StreamReader sr)
-		{
-			this.Brand= sr.ReadLine();
-			this.Model = sr.ReadLine();
-			this.Price = int.Parse(sr.ReadLine());
-			this.Colour = sr.ReadLine();
-			this.Size = sr.ReadLine();
-			this.Motherboard = sr.ReadLine();
-			this.CPU = sr.ReadLine();
-			this.Ram = int.Parse(sr.ReadLine());
-		}
-	public void writeToFile(StreamWriter sw)
-		{
-			sw.WriteLine(Brand);
-			sw.WriteLine(Model);
-			sw.WriteLine(Price);
-			sw.WriteLine(Colour);
-			sw.WriteLine(Size);
-			sw.WriteLine(Motherboard);
-			sw.WriteLine(CPU);
-			sw.WriteLine(Ram);
-		}
-		public void Print(MessageSender sender)
-		{
-			sender(PrintContent);
-		}
 
-		public void Draw(string something, MessageSender sender)
-		{
-			sender("Showed System Unit " + something);
-		}
-	}
+        }
+        public SystemUnit(string _brand, string _model, int _price) : this(_brand, _model, _price, "black", "intelcorei14.1", "mid-tower", "asusgromyako", 32)
+        {
+
+        }
+        public SystemUnit(string _brand, string _model, int _price, string _colour, string _cpu, string _size, string _motherboard, int _ram)
+        {
+            Brand = _brand;
+            Model = _model;
+            Price = _price;
+            Colour = _colour;
+            Size = _size;
+            Motherboard = _motherboard;
+            CPU = _cpu;
+            Ram = _ram;
+        }
+        public void readFromFile(StreamReader sr)
+        {
+            this.Brand = sr.ReadLine();
+            this.Model = sr.ReadLine();
+            this.Price = int.Parse(sr.ReadLine());
+            this.Colour = sr.ReadLine();
+            this.Size = sr.ReadLine();
+            this.Motherboard = sr.ReadLine();
+            this.CPU = sr.ReadLine();
+            this.Ram = int.Parse(sr.ReadLine());
+        }
+        public void writeToFile(StreamWriter sw)
+        {
+            sw.WriteLine(Brand);
+            sw.WriteLine(Model);
+            sw.WriteLine(Price);
+            sw.WriteLine(Colour);
+            sw.WriteLine(Size);
+            sw.WriteLine(Motherboard);
+            sw.WriteLine(CPU);
+            sw.WriteLine(Ram);
+        }
+        public void Print(MessageSender sender)
+        {
+            sender(PrintContent);
+        }
+
+        public void Draw(string something, MessageSender sender)
+        {
+            sender("Showed System Unit " + something);
+        }
+    }
 }
+
